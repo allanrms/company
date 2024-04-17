cd441081c5d704d4602dccb679493f338840318f
Allan Ramos <allan.charlys@gmail.com>
2024-04-17 08:48:04 -0300

    adicionando configurações

---
+++ models.py
@@ -0,0 +1,13 @@
+from common.models import AuditLogBaseModel
+
+
+# Create your models here.
+
+class Company(AuditLogBaseModel):
+    pass
+
+class Secretary(AuditLogBaseModel):
+    pass
+
+class Department(AuditLogBaseModel):
+    pass
